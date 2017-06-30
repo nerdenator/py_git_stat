68c6a166aa1e4ebaea9186e8ee9928a7835bd225
am034402 <aaron.mcruer@cerner.com>
2017-06-30 14:20:09 -0500

    moved length finders into their own function

--- py_git_stat/__init__.py
+++ py_git_stat/__init__.py
@@ -38,13 +38,14 @@ def py_git_stat():
     print_row_separators(term_width)
 
     # let's get the longest repo name and make that the width of the first column in the table.
-    longest = (len(max(status_results, key=len)))+1
-    longest_repo_branch = (len(max(status_results.values()[1], key=len)))+1
-    print('{0: <{longest}}|{1}'.format('git repository', 'repo branch', longest=longest))
+    longest_repo_name = (get_longest_element(status_results)) + 1
+    longest_repo_branch = (get_longest_element(status_results.values()[1])) + 1
+    print('{0: <{longest_repo_name}}|{1}'.format('git repository', 'repo branch', longest_repo_name=longest_repo_name))
     print_row_separators(term_width)
     for repo_name, repo_info in status_results.iteritems():
-        print ('{0: <{longest}}|{1: <{longest_repo_branch}}'.format(repo_name, repo_info[0], longest=longest,
-                                                                    longest_repo_branch=longest_repo_branch))
+        print ('{0: <{longest_repo_name}}|{1: <{longest_repo_branch}}'.format(repo_name, repo_info[0],
+                                                                              longest_repo_name=longest_repo_name,
+                                                                              longest_repo_branch=longest_repo_branch))
 
 
 def print_row_separators(term_size):
@@ -55,3 +56,13 @@ def print_row_separators(term_size):
     """
     # print out that many characters
     print('-' * term_size)
+
+
+def get_longest_element(focus):
+    """
+    cycles thru the status_results dictionary in the indicated key or field list and finds the
+    longest piece of text in it
+    :param focus: the key or field in value list that is being searched for longest value
+    :return: the longest value
+    """
+    return (len(max(focus, key=len)))
