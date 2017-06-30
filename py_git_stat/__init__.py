be148d5a64472e120e7b7e1a6b83704511019451
am034402 <aaron.mcruer@cerner.com>
2017-06-30 12:03:45 -0500

    trying to convert status_results to a dictionary

--- py_git_stat/__init__.py
+++ py_git_stat/__init__.py
@@ -1,6 +1,8 @@
 import os
 from pathlib2 import Path
 import subprocess
+from backports.shutil_get_terminal_size import get_terminal_size
+
 
 def py_git_stat():
     # get the current working directory
@@ -22,11 +24,19 @@ def py_git_stat():
     # now that we have only top-level git repos, let's execute git status on all of them, then store the
     # command output
 
-    status_results = []
+    status_results = {}
     for git_repo in git_repos:
         os.chdir(str(git_repo.resolve()))
         result = subprocess.check_output(['git', 'status'])
-        status_results.append((git_repo.name, result.split('\n')))
+        status_results.append({git_repo.name, result})
+
+    # let's print out some headers for the table. first, get width of the terminal
+    term_width = get_terminal_size()[0]
+    # print out that many characters
+    print '-' * term_width
+
+    # let's get the longest repo name and make that the width of the first column in the table.
+
 
     for status_result in status_results:
         print status_result
