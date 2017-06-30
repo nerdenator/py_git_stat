60f336c115866fc80dcfef9542e5f75965a85979
am034402 <aaron.mcruer@cerner.com>
2017-06-30 14:47:29 -0500

    test scaffolding added.

---
+++ py_git_stat/tests/tests.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+import py_git_stat
+import os
+
+
+class TestPyGitStat(TestCase):
+    def test_output_in_dir_with_git_repos(self):
+        pass
+
+    def test_output_in_dir_without_git_repos(self):
+        pass
